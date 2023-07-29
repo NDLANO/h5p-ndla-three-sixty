451217ab3c34a8a305c8fa3295ad83086f9333e3
Oliver Tacke <o.tacke@posteo.de>
2023-07-29 17:10:46 +0200

    Revert "Change element handling"
    This reverts commit 6302c19127d00dcfc9170ae83fa73911a3d452fd.

--- src/scripts/h5p-ndla-three-sixty.js
+++ src/scripts/h5p-ndla-three-sixty.js
@@ -1,8 +1,10 @@
-import { DEFAULT_RATIO, FOV_PANORAMA, FOV_SPHERE } from '@services/constants';
+import {
+  DEFAULT_RATIO, DEFAULT_ROTATION_ORDER, FOV_PANORAMA, FOV_SPHERE
+} from '@services/constants';
 
+import PositionControls from '@scripts/position-controls';
 import Util from '@services/util';
 import Initialization from '@mixins/initialization';
-import ElementHandling from '@mixins/element-handling';
 import SphereHandling from '@mixins/sphere-handling';
 
 export default class NDLAThreeSixty extends H5P.EventDispatcher {
@@ -24,7 +26,7 @@ export default class NDLAThreeSixty extends H5P.EventDispatcher {
     super();
 
     Util.addMixins(
-      NDLAThreeSixty, [Initialization, ElementHandling, SphereHandling]
+      NDLAThreeSixty, [Initialization, SphereHandling]
     );
 
     this.options = Util.extend({
@@ -182,6 +184,104 @@ export default class NDLAThreeSixty extends H5P.EventDispatcher {
   }
 
   /**
+   * Add element to "CSS 3d world".
+   * @param {HTMLElement} element Element to add.
+   * @param {object} startPosition Start position.
+   * @param {boolean} enableControls If true, enable controls.
+   * @returns {H5P.ThreeJS.CSS3DObject} ThreeJS CSS3DObject.
+   */
+  add(element, startPosition, enableControls) {
+    let threeElement;
+
+    if (element.classList.contains('render-in-3d')) {
+      threeElement = new H5P.ThreeJS.CSS3DObject(element);
+      threeElement.is3d = true;
+    }
+    else {
+      threeElement = new H5P.ThreeJS.CSS2DObject(element);
+    }
+
+    this.threeElements.push(threeElement);
+
+    // Reset HUD values
+    element.style.left = '0';
+    element.style.top = '0';
+
+    // Move camera to element when tabbing
+    element.addEventListener('focus', (event) => {
+      if (!event.defaultPrevented && !this.preventCameraMovement) {
+        this.setCameraPosition(
+          -threeElement.rotation.y, threeElement.rotation.x
+        );
+      }
+
+      this.setPreventCameraMovement(false);
+    }, false);
+
+    if (enableControls) {
+      const elementControls = new PositionControls(element);
+
+      // Relay and supplement startMoving event
+      elementControls.on('movestart', (event) => {
+        // Set camera start position
+        elementControls.startY = -threeElement.rotation.y;
+        elementControls.startX = threeElement.rotation.x;
+
+        this.preventDeviceOrientation = true;
+        this.trigger(event);
+      });
+
+      // Update element position according to movement
+      elementControls.on('move', (event) => {
+        NDLAThreeSixty.setElementPosition(threeElement, {
+          yaw: elementControls.startY + event.alpha,
+          pitch: elementControls.startX - event.beta
+        });
+      });
+
+      // Relay and supplement stopMoving event
+      elementControls.on('movestop', (event) => {
+        event.data = {
+          target: element,
+          yaw: -threeElement.rotation.y,
+          pitch: threeElement.rotation.x
+        };
+
+        this.preventDeviceOrientation = false;
+        this.trigger(event);
+      });
+    }
+
+    // Set initial position
+    NDLAThreeSixty.setElementPosition(threeElement, startPosition);
+
+    this.cssScene.add(threeElement);
+
+    return threeElement;
+  }
+
+  /**
+   * Remove element from "CSS world".
+   * @param {H5P.ThreeJS.CSS3DObject} threeElement Element to be removed.
+   */
+  remove(threeElement) {
+    this.threeElements.splice(this.threeElements.indexOf(threeElement), 1);
+    this.cssScene.remove(threeElement);
+  }
+
+  /**
+   * Find the threeElement for the given element.
+   * TODO: Move into a separate collection handling class
+   * @param {Element} element Element.
+   * @returns {H5P.ThreeJS.CSS3DObject} Corresponding ThreeJS CSS3DObject.
+   */
+  find(element) {
+    return this.threeElements.find((threeElement) => {
+      return threeElement.element === element;
+    });
+  }
+
+  /**
    * Will re-create the world sphere. Useful after changing sourceElement
    * or segment number.
    *
@@ -298,5 +398,30 @@ export default class NDLAThreeSixty extends H5P.EventDispatcher {
       this.triggerFirstRenderEvent = false;
       this.trigger('firstrender');
     }
+  }
+
+  /**
+   * Set element's position in the 3d world, always facing the camera.
+   * TODO: Check why this was build static and why external code should use it.
+   * @param {H5P.ThreeJS.CSS3DObject} threeElement CSS3DObject.
+   * @param {object} position Position object.
+   * @param {number} position.yaw Radians from 0 to Math.PI*2 (0-360).
+   * @param {number} position.pitch Radians from -Math.PI/2 to Math.PI/2 (-90-90).
+   */
+  static setElementPosition(threeElement, position) {
+    const radius = 800; // Default radius of 800
+
+    threeElement.position.x = radius *
+      Math.sin(position.yaw) * Math.cos(position.pitch);
+
+    threeElement.position.y = radius *
+      Math.sin(position.pitch);
+
+    threeElement.position.z = -radius *
+      Math.cos(position.yaw) * Math.cos(position.pitch);
+
+    threeElement.rotation.order = DEFAULT_ROTATION_ORDER;
+    threeElement.rotation.y = -position.yaw;
+    threeElement.rotation.x = +position.pitch;
   }
 }
